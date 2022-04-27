bc6a369394d2aaf4183b1bb117f3497530c394c9
gercamjr <gercamjr.dev@gmail.com>
2022-04-27 15:27:06 -0500

    creating api and router

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.json())
+app.use(express.static('../public'))
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://teamtitan:fOSt21g0gCbxhYCM@abc.qtywt.mongodb.net/ABC?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+app.get('/search', (req, res) => {
+
+    async function showAllBeers() {
+        try {
+            await client.connect(err => {
+                const collection = client.db("ABC").collection("Beers");
+                // perform actions on the collection object
+                const beers = await collection.find()
+                res.send(beers)
+                client.close()
+              })
+        } catch (e) {
+            console.log(e.message)
+        }
+    }
+    showAllBeers()
+})
+
+app.get('/search/:filter/:query', (req, res) => {
+    async function searchBeers() {
+        const query = req.params.query
+        const filter = req.params.filter
+        try {
+            await client.connect()
+            const collection = client.db('ABC').collection('Beers')
+            const cursorArr = await collection.find({[filter]: query})
+            res.send(cursorArr)
+            await client.close()
+        } catch (err) {
+            console.log(err)
+            res.sendStatus(400)
+        }
+    }
+    searchBeers()
+})
+
+app.listen(port)
+
+
+
