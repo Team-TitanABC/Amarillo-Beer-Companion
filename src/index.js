330fe550fba0bf52366385d47aea49e06e7b0509
gercamjr <gercamjr.dev@gmail.com>
2022-04-27 10:05:19 -0500

    restructuring project

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://teamtitan:fOSt21g0gCbxhYCM@abc.qtywt.mongodb.net/ABC?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 
 async function showAllBeers() {
     try {
-        
+        const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
     } catch (e) {
         console.log(e.message)
     }
