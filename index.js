5f8176d6aee77f6b479780066134161c82dd7096
gercamjr <gercamjr.dev@gmail.com>
2022-04-28 10:21:43 -0500

    took out semicolon

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.get("/search/:filter/:query", (req, res) => {
     try {
       await client.connect();
       const collection = client.db("ABC").collection("Beers");
-      const cursorArr = await collection.find({ [filter]: query }).toArray();
+      const cursorArr = await collection.find({ [filter]: query }).toArray()
       res.send(cursorArr);
       await client.close();
     } catch (err) {
