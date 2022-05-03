33b53ab070cac84ea0896bb5c6c08c8587b1e087
IsraeljGalan <ijgalan@amarillocollege.com>
2022-05-03 15:11:54 -0500

    updated index.js

--- ABC/backend/index.js
+++ ABC/backend/index.js
@@ -5,43 +5,44 @@ const port = 3000;
 app.use(express.json());
 app.use(express.static("../public"));
 
+const dotenv = require("dotenv")
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  "mongodb+srv://teamtitan:fOSt21g0gCbxhYCM@abc.qtywt.mongodb.net/ABC?retryWrites=true&w=majority";
+const uri = process.env.MONGOPASSWORD
+
 const client = new MongoClient(uri);
 
 app.get("/search", (req, res) => {
   async function showAllBeers() {
-	try {
-	  await client.connect();
-	  console.log("connection to mongodb was established");
-	  const collection = client.db("ABC").collection("Beers");
-	  // perform actions on the collection object
-	  const beers = await collection.find().toArray();
-	  res.send(beers);
-	  await client.close();
-	} catch (e) {
-	  console.log(e.message);
-	  res.sendStatus(400);
-	}
+    try {
+      await client.connect();
+      console.log("connection to mongodb was established");
+      const collection = client.db("ABC").collection("Beers");
+      // perform actions on the collection object
+      const beers = await collection.find().toArray();
+      res.send(beers);
+      await client.close();
+    } catch (e) {
+      console.log(e.message);
+      res.sendStatus(400);
+    }
   }
   showAllBeers();
 });
 
 app.get("/search/:filter/:query", (req, res) => {
   async function searchBeers() {
-	const query = req.params.query.split("_").join(" ")
-	const filter = req.params.filter;
-	try {
-	  await client.connect();
-	  const collection = client.db("ABC").collection("Beers");
-	  const cursor = await collection.findOne({ [filter]: query })
-	  res.send(cursor);
-	  await client.close();
-	} catch (err) {
-	  console.log(err);
-	  res.sendStatus(400);
-	}
+    const query = req.params.query;
+    const filter = req.params.filter;
+    try {
+      await client.connect();
+      const collection = client.db("ABC").collection("Beers");
+      const cursorArr = await collection.find({ [filter]: query }).toArray()
+      res.send(cursorArr);
+      await client.close();
+    } catch (err) {
+      console.log(err);
+      res.sendStatus(400);
+    }
   }
   searchBeers();
 });
@@ -51,4 +52,3 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port);
-
