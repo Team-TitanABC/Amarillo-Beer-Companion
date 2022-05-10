4f9444a68cbc6bfbe46bb59376ff812927855163
gercamjr <gercamjr.dev@gmail.com>
2022-05-10 11:42:09 -0500

    init node project.json

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 const express = require("express");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT
 
 app.use(express.json());
 app.use(express.static("../frontend/public"));
