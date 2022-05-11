8132b5a76e1dffdaaec331a97ec58859db41d8c5
David M. Wilson <d0274140@amarillocollege.com>
2022-05-11 15:40:32 -0500

    made A LOT of CSS changes

---
+++ frontend/public/background.js
@@ -0,0 +1,6 @@
+window.setTimeout(loadBackground, 24000) 
+
+function loadBackground() {
+	document.getElementById("myVideo").style.display = "none"
+	// document.querySelector("main").style.backgroundImage = "background: url(../images/ABC-Background.jpg)"
+}
