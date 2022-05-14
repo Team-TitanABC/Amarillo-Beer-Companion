ba3738a049b87e124448ad7ddfb4a846e1fa24df
David M. Wilson <d0274140@amarillocollege.com>
2022-05-14 13:34:29 -0500

    updated stuff.

--- frontend/public/background.js
+++ frontend/public/background.js
@@ -4,3 +4,15 @@ function loadBackground() {
 	document.getElementById("myVideo").style.display = "none"
 	// document.querySelector("main").style.backgroundImage = "background: url(../images/ABC-Background.jpg)"
 }
+
+
+// 
+// function change() {
+//   const elements = document.querySelectorAll("#main-div");
+//   for (let element of elements) {
+// 	element.classList.toggle("transformed-state");
+//   }
+// }
+
+// const changeButton = document.querySelector("#change");
+// changeButton.addEventListener("click", change);
